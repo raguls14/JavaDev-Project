b60480b35591e121c60848a804f6d84cdb18e479
raguls14 <ragulmahes1411@gmail.com>
2023-09-13 21:36:59 +0530

    first commit

---
+++ Demo.java
@@ -0,0 +1,13 @@
+class Demo
+{
+    public static void main(String []args)
+    {
+        int i=1;
+        ++i;
+        while(i<=4)
+        {
+            System.out.println("The value of i is: " + i);
+            ++i;
+        }
+    }
+}
---
+++ Hello.java
@@ -0,0 +1,11 @@
+class Hello
+{
+    public static void main(String []args)
+    {
+        byte b=12;
+        byte c=10;
+        boolean r=b>c;
+        System.out.println(r);
+        System.out.println("Hello");
+    }
+}
